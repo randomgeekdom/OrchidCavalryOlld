7c717b222d5f6a6426eafa0b93ea10ee5944c812
randomgeekdom <randomgeekdom@gmail.com>
2022-04-06 09:18:46 -0500

    Add project files.

---
+++ Orchid.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Orchid.Web.Models;
+
+namespace Orchid.Web.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+
+        }
+    }
+}
